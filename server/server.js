008d04f25ff97c049ffa282922d51f167acad252
Kristian Slosar <slosar.kristian@gmail.com>
2016-12-23 20:13:31 +0100

    Added tests for POST todos route and added GET todos route

--- server/server.js
+++ server/server.js
@@ -7,22 +7,39 @@ const {User} = require('./models/user');
 
 var app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-// POST ROUTE
+// POST todo ROUTE
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
+
   todo.save().then((doc) => {
+    // console.log(doc);
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
   });
 });
 
+// GET todos ROUTE
+app.get('/todos', (req, res) => {
+  Todo.find({}).then((todos) => {
+    console.log(todos);
+    res.send({todos});
+  }, (e) => {
+    console.log(e);
+    res.status(400).send(e);
+  });
+});
+
+
 
 //Starting express server
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+
+module.exports ={app};
