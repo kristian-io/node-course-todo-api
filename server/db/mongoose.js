e6abaedfb246946aaf2b350ecaa85549cd686f49
Kristian Slosar <slosar.kristian@gmail.com>
2016-12-28 18:44:02 +0100

    Added connection to external db from mlab.com in mongoose.js and added folder /private to gitignore

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,12 @@
 var mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var dblogin = require('./../../private/dblogin.js');
 
+// console.log(dblogin.user, dblogin.password);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://${dblogin.user}:${dblogin.password}@ds141328.mlab.com:41328/data`);
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
