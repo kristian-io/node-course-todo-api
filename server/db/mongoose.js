1597dcdddf480122723161c6d5a308ff1359639c
Kristian Slosar <slosar.kristian@gmail.com>
2016-12-28 19:24:49 +0100

    ?

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var dblogin = require('./../../private/dblogin.js');
 
-// console.log(dblogin.user, dblogin.password);
+// console.log(dblogin.user, dblogin.password);!!!
 
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://${dblogin.user}:${dblogin.password}@ds141328.mlab.com:41328/data`);
