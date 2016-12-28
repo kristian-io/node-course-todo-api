498605fd928660f00c4c405a393799d0854c9e9e
Kristian Slosar <slosar.kristian@gmail.com>
2016-12-28 20:40:30 +0100

    added remote DB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ let password='*mlab123456';
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://${dblogin.user}:${dblogin.password}@ds141328.mlab.com:41328/data`);
+mongoose.connect(`mongodb://${user}:${password}@ds141328.mlab.com:41328/data`);
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {
