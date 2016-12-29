fce8eb728817d8922eb9879c8520d29d38c91d6a
Kristian Slosar <slosar.kristian@gmail.com>
2016-12-29 20:18:04 +0100

    Authentication middleware amd GET /users/me

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,8 +5,8 @@ let password='T_u!6phEcraY';
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://${user}:${password}@ds141358.mlab.com:41358/data`);
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect(`mongodb://${user}:${password}@ds141358.mlab.com:41358/data`);
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -109,6 +110,12 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+
+// GET /users/me ROUTE
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 });
 
 //Starting express server
