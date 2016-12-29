9a2f01e6aba8e1edc8d3681fbaf3920b3bc22559
Kristian Slosar <slosar.kristian@gmail.com>
2016-12-29 21:09:07 +0100

    Added password hashing before saving pwd to DB

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -67,6 +68,22 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.pre('save', function (next) {
+  var user = this;
+  if (user.isModified('password')) {
+      //user.password
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+          // console.log('setting up password to:', hash);
+          user.password = hash;
+          next()
+        });
+      });
+  } else {
+    console.log('password was not modified.');
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
